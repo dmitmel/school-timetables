9d72d0a97c4fca99673bf4b809f36cda06273e9b
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2019-11-03 15:31:14 +0200

    add lesson data files

--- render.js
+++ render.js
@@ -4,6 +4,7 @@ const fs = require('fs-extra');
 const path = require('path');
 const nunjucks = require('nunjucks');
 const klawSync = require('klaw-sync');
+const sprintf = require('sprintf-js');
 
 const TEMPLATES_DIR = path.join(__dirname, 'templates');
 const DATA_FILES_DIR = path.join(__dirname, 'data');
@@ -27,11 +28,26 @@ staticFiles.forEach(({ path: filePath, stats }) => {
 let lessonTimes = fs.readJsonSync(
   path.join(DATA_FILES_DIR, 'lesson-times', 'Basis.json'),
 );
+let lessons = fs.readJsonSync(
+  path.join(
+    DATA_FILES_DIR,
+    'lessons',
+    'Basis',
+    '2019-2020',
+    'I семестр',
+    '9-A.json',
+  ),
+);
 
 let env = new nunjucks.Environment(
   new nunjucks.FileSystemLoader(TEMPLATES_DIR),
 );
+env.addFilter('format', function format(formatStr, ...args) {
+  return sprintf.vsprintf(formatStr, args);
+});
+
 let renderedHtml = env.render('timetable.njk', {
   lessonTimes,
+  lessons,
 });
 fs.writeFileSync(path.join(RENDERED_FILES_DIR, 'index.html'), renderedHtml);
