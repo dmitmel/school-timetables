1b1b0e520b7270771414bc89369731264bcff70c
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2019-11-05 00:02:35 +0200

    add html minification

--- render.js
+++ render.js
@@ -6,6 +6,7 @@ const nunjucks = require('nunjucks');
 const klawSync = require('klaw-sync');
 const sprintf = require('sprintf-js');
 const materialColors = require('material-colors');
+const htmlMinifier = require('html-minifier');
 
 const TEMPLATES_DIR = path.join(__dirname, 'templates');
 const DATA_FILES_DIR = path.join(__dirname, 'data');
@@ -15,7 +16,7 @@ const RENDERED_FILES_DIR = path.join(__dirname, 'rendered');
 fs.ensureDirSync(RENDERED_FILES_DIR);
 fs.emptyDirSync(RENDERED_FILES_DIR);
 
-klawSync(STATIC_FILES_DIR).forEach(({path: filePath, stats}) => {
+klawSync(STATIC_FILES_DIR).forEach(({ path: filePath, stats }) => {
   let relativePath = path.relative(STATIC_FILES_DIR, filePath);
   let pathInRendered = path.join(RENDERED_FILES_DIR, relativePath);
   if (stats.isDirectory()) {
@@ -45,7 +46,7 @@ let lessonColors = fs.readJsonSync(
   path.join(DATA_FILES_DIR, 'lesson-colors.json'),
 );
 Object.keys(lessonColors).forEach(lessonName => {
-  let {back, fore} = lessonColors[lessonName];
+  let { back, fore } = lessonColors[lessonName];
   lessonColors[lessonName] = {
     back: flattenedMaterialColors[back],
     fore: flattenedMaterialColors[fore],
@@ -65,9 +66,28 @@ env.addFilter('format', function format(formatStr, ...args) {
 
 const LESSON_DATA_FILES_DIR = path.join(DATA_FILES_DIR, 'lessons');
 
+function renderTemplate({ name, renderedName, context }) {
+  console.log(`rendering '${name}' to '${renderedName}'`);
+
+  let renderedText = env.render(name, {
+    relativeRoot: path.relative(path.dirname(renderedName), '.') || '.',
+    ...context,
+  });
+
+  renderedText = htmlMinifier.minify(renderedText, {
+    collapseWhitespace: true,
+    removeComments: true,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    useShortDoctype: true,
+  });
+
+  fs.writeFileSync(path.join(RENDERED_FILES_DIR, renderedName), renderedText);
+}
+
 function renderLessonFilesDir(dirPath, dataDirNames) {
   let relativeDirPath = path.relative(LESSON_DATA_FILES_DIR, dirPath);
-  let templateRelativeRoot = path.relative(relativeDirPath, '.') || '.';
   fs.ensureDirSync(path.join(RENDERED_FILES_DIR, relativeDirPath));
 
   let contents = [];
@@ -75,38 +95,36 @@ function renderLessonFilesDir(dirPath, dataDirNames) {
     let fullPath = path.join(dirPath, name);
     if (fs.lstatSync(fullPath).isDirectory()) {
       renderLessonFilesDir(fullPath, [...dataDirNames, name]);
-      contents.push({name, isDir: true});
+      contents.push({ name, isDir: true });
     } else {
       let extName = path.extname(name);
       if (extName === '.json') {
         let baseName = path.basename(name, extName);
         let lessons = fs.readJsonSync(fullPath);
-        let timetableHtml = env.render('timetable.njk', {
-          relativeRoot: templateRelativeRoot,
-          dirNames: dataDirNames,
-          name: baseName,
-          lessonColors,
-          lessonTimes,
-          lessons,
+        renderTemplate({
+          name: 'timetable.njk',
+          renderedName: path.join(relativeDirPath, `${baseName}.html`),
+          context: {
+            dirNames: dataDirNames,
+            name: baseName,
+            lessonColors,
+            lessonTimes,
+            lessons,
+          },
         });
-        fs.writeFileSync(
-          path.join(RENDERED_FILES_DIR, relativeDirPath, `${baseName}.html`),
-          timetableHtml,
-        );
-        contents.push({name: baseName, isDir: false});
+        contents.push({ name: baseName, isDir: false });
       }
     }
   });
 
-  let indexHtml = env.render('directory-index.njk', {
-    relativeRoot: templateRelativeRoot,
-    dirNames: dataDirNames,
-    contents,
+  renderTemplate({
+    name: 'directory-index.njk',
+    renderedName: path.join(relativeDirPath, 'index.html'),
+    context: {
+      dirNames: dataDirNames,
+      contents,
+    },
   });
-  fs.writeFileSync(
-    path.join(RENDERED_FILES_DIR, relativeDirPath, 'index.html'),
-    indexHtml,
-  );
 }
 
 renderLessonFilesDir(LESSON_DATA_FILES_DIR, []);
