1cdd979d2e76a89db4858650b41ff84e18c37726
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2019-11-09 17:51:53 +0200

    add smaller title

--- templates/components/Header.js
+++ templates/components/Header.js
@@ -2,5 +2,9 @@ const h = require('../hyperscript');
 const CurrentDirectory = require('./CurrentDirectory');
 
 module.exports = function Header({ dirs, disableLast }) {
-  return h('header', CurrentDirectory({ dirs, disableLast }));
+  return h(
+    'header',
+    h('p', { id: 'title' }, 'Індекс шкільних розкладів уроків'),
+    CurrentDirectory({ dirs, disableLast }),
+  );
 };
